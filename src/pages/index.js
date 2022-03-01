999d98a454c5eb1bec4c912ef70ca2ba4441b530
Andy Terwilliger <andy@terwilliger1@gmail.com>
2022-02-28 19:42:38 -0600

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+function Home(props) {
+  return (
+  <body id = 'home-body' style={{ minHeight: '100vh'}}>
+  <div id = 'chicago-img-container'>
+ 
+ </div>
+ <br/>
+ <br/>
+      <div>
+
+          <h1 id = 'home-h1'>
+             Hi, I'm Andy.
+  </h1> <br/> <br/>
+          <h2 id = 'home-h2'>
+             I'm a developer and problem solver. 
+  </h2>
+      </div>
+      </body>
+  )
 }
+
+
+export default Home;
