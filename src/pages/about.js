d3be0242259a81ff6504186bb8599189cb769af3
Andy Terwilliger <andy@terwilliger1@gmail.com>
2024-04-03 20:48:39 -0500

    Updated resume file

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,7 +19,7 @@ function About(props){
           I began my career in web development during the midst of the pandemic after spending the majority of my adult career as a firefighter. By doing so, I have found a passion for creating
           well-designed, clean, and functional web applications.</p> <p>I have two years of experience as in a professional agency setting, where I have worked on a variety of projects for Fortune 500 clients, including high traffic promotions for e-commerce websites. I am proficient in HTML, CSS, JavaScript, React, and Node.js, and I am always looking to learn more. I am currently seeking a full-time position as a front-end developer, and I am excited to bring my unique perspective and problem-solving skills to a new team.</p>
     </ReactBootStrap.Card.Text>
-   <a target = '_blank'href='https://cat-collector-bucket2.s3.amazonaws.com/ATerwilligerResume.pdf' download><ReactBootStrap.Button id='resume-btn'>Download Resume</ReactBootStrap.Button></a>
+   <a target = '_blank'href='https://cat-collector-bucket2.s3.amazonaws.com/ANDYTERWILLIGER_RESUME_Dev_2024.pdf' download><ReactBootStrap.Button id='resume-btn'>Download Resume</ReactBootStrap.Button></a>
   </ReactBootStrap.Card.Body>
 </ReactBootStrap.Card>
 
